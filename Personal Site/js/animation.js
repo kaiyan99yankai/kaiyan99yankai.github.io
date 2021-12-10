7546af6b7fb7ac02d62d1859d332b9fa97dc7a6a
kaiyan99yankai <60974418+kaiyan99yankai@users.noreply.github.com>
2021-12-10 18:38:29 -0500

    Delete Personal Site directory

--- Personal Site/js/animation.js
+++
@@ -1,7 +0,0 @@
-var back_image = document.getElementsByTagName('P');
-var i = 0;
-for(i = 0; i < 3; i++){
-    window.setTimeout(function(){
-        back_image[i].style.visibility = 'visible';
-    }, 100);
-};
